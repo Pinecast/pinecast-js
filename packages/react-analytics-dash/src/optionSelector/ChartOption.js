2948ad8a729c3da573f060346bf6b0c272cbd704
Matt Basta <me@mattbasta.com>
2017-01-28 12:14:08 -0800

    Add aria attributes to chart option selector

--- packages/react-analytics-dash/src/optionSelector/ChartOption.js
+++ packages/react-analytics-dash/src/optionSelector/ChartOption.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 const ChartOption = ({name, selected, setSelected, value}) =>
     <a
+        aria-checked={value === selected ? 'true' : 'false'}
         className={`chart-option ${value !== selected ? '' : 'is-selected'}`}
+        role='radio'
         onClick={() => setSelected(value)}
     >
         {name}
--- packages/react-analytics-dash/src/optionSelector/index.js
+++ packages/react-analytics-dash/src/optionSelector/index.js
@@ -5,7 +5,7 @@ import ChartOption from './ChartOption';
 
 const ChartOptionSelector = ({defaultSelection, onChange, options}) => {
     const keys = Object.keys(options);
-    return <div className='chart-option-selector'>
+    return <div className='chart-option-selector' role='radiogroup'>
         {keys.length > 1 &&
             keys.map(option =>
                 <ChartOption key={option}
