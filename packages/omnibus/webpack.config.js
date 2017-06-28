9cc91374df2dce6c761a5221ff3ceee3d498129f
Matt Basta <me@mattbasta.com>
2017-06-28 15:02:22 -0700

    Upgrades; remove unnecessary packages

--- packages/omnibus/webpack.config.js
+++ packages/omnibus/webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
+const path = require('path');
 
-var webpack = require('webpack');
+const webpack = require('webpack');
 
 
 module.exports = {
@@ -47,16 +47,32 @@ module.exports = {
         ],
     },
     externals: [
-        function(context, request, callback) {
+        (context, request, callback) => {
             switch (request) {
+                // For Node compatibility
                 case 'fs':
                 case 'net':
                     return callback(null, 'null');
                 case 'tty':
                     return callback(null, '{isatty:function() {}}');
+
+                // Removing junk that doesn't need a package.
                 case 'bluebird':
                     // For react-google-charts
                     return callback(null, 'Promise');
+                case 'function-bind':
+                    return callback(null, 'Function.prototype.bind');
+                case 'is-array':
+                case 'isarray':
+                    return callback(null, 'Array.isArray');
+                case 'is-function':
+                    return callback(null, 'function(x) {return typeof x === "function";}');
+                case 'object.assign':
+                case 'object-assign':
+                    return callback(null, 'Object.assign');
+                case 'object-keys':
+                    return callback(null, '(function() {var x = Object.keys(); x.shim = x; return x;}())');
+
                 default:
                     return callback();
             }
