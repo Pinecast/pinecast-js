cb39365bbc0872a79ab1106a43a5b4d32138320f
Matt Basta <me@mattbasta.com>
2018-03-29 20:19:27 -0700

    Cleanup build on omnibus

--- packages/omnibus/webpack.config.js
+++ packages/omnibus/webpack.config.js
@@ -3,100 +3,107 @@ const path = require('path');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  devtool: 'cheap-source-maps',
-  entry: {
-    app: [
-      // 'babel-polyfill',
-      './src/index.js',
-    ],
-  },
-  resolve: {
-    mainFields: ['jsnext:main', 'main'],
-    modules: [
-      path.resolve(__dirname, 'vendor'),
-      path.resolve(__dirname, '../../node_modules'),
-      'node_modules',
-    ],
-    // alias: {
-    //   '@mattbasta/browser-id3-writer$': '/opt/browser-id3-writer/src/ID3Writer.js',
-    // },
-  },
-  cache: false,
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'ui-omnibus.js',
-  },
-  plugins: [
+module.exports = env => {
+  const plugins = [
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }),
-    new webpack.LoaderOptionsPlugin({minimize: true}),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new MinifyPlugin(
-      {
-        mangle: {
-          blacklist: ['Buffer'],
+  ];
+
+  if (env === 'prod') {
+    plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"',
+      }),
+    );
+    plugins.push(new webpack.LoaderOptionsPlugin({minimize: true}));
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+    plugins.push(
+      new MinifyPlugin(
+        {
+          mangle: {
+            blacklist: ['Buffer'],
+          },
         },
-      },
-      {sourceMap: 'cheap-source-maps'},
-    ),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        {sourceMap: 'cheap-source-maps'},
+      ),
+    );
+  }
+  return {
+    devtool: 'cheap-source-maps',
+    entry: {
+      app: [
+        // 'babel-polyfill',
+        './src/index.js',
+      ],
+    },
+    resolve: {
+      mainFields: ['jsnext:main', 'main'],
+      modules: [
+        path.resolve(__dirname, 'vendor'),
+        path.resolve(__dirname, '../../node_modules'),
+        'node_modules',
+      ],
+    },
+    cache: false,
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: 'ui-omnibus.js',
+    },
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    externals: [
+      (context, request, callback) => {
+        switch (request) {
+          // For Node compatibility
+          case 'fs':
+          case 'net':
+            return callback(null, 'null');
+          case 'tty':
+            return callback(null, '{isatty:function() {}}');
+
+          // Removing junk that doesn't need a package.
+          case 'bluebird':
+            // For react-google-charts
+            return callback(null, 'Promise');
+          case 'function-bind':
+            return callback(null, 'Function.prototype.bind');
+          case 'is-array':
+          case 'isarray':
+            return callback(null, 'Array.isArray');
+          case 'is-finite':
+            return callback(
+              null,
+              'function(x) {return typeof x !== "number" && !isNaN(x) && x !== Infinity && x !== -Infinity;}',
+            );
+          case 'is-function':
+            return callback(null, 'function(x) {return typeof x === "function";}');
+          case 'object.assign':
+          case 'object-assign':
+            return callback(null, 'Object.assign');
+          case 'object-keys':
+            return callback(
+              null,
+              '(function() {var x = Object.keys.bind(Object); x.shim = x; return x;}())',
+            );
+
+          default:
+            return callback();
+        }
       },
     ],
-  },
-  externals: [
-    (context, request, callback) => {
-      switch (request) {
-        // For Node compatibility
-        case 'fs':
-        case 'net':
-          return callback(null, 'null');
-        case 'tty':
-          return callback(null, '{isatty:function() {}}');
-
-        // Removing junk that doesn't need a package.
-        case 'bluebird':
-          // For react-google-charts
-          return callback(null, 'Promise');
-        case 'function-bind':
-          return callback(null, 'Function.prototype.bind');
-        case 'is-array':
-        case 'isarray':
-          return callback(null, 'Array.isArray');
-        case 'is-finite':
-          return callback(
-            null,
-            'function(x) {return typeof x !== "number" && !isNaN(x) && x !== Infinity && x !== -Infinity;}',
-          );
-        case 'is-function':
-          return callback(null, 'function(x) {return typeof x === "function";}');
-        case 'object.assign':
-        case 'object-assign':
-          return callback(null, 'Object.assign');
-        case 'object-keys':
-          return callback(
-            null,
-            '(function() {var x = Object.keys.bind(Object); x.shim = x; return x;}())',
-          );
-
-        default:
-          return callback();
-      }
-    },
-  ],
+  };
 };
